f900ec6bab72de4ccdad883aedb0eb09748ebe25
landon burnier <landonburnier@gmail.com>
2016-10-11 17:59:37 -0400

    ghetto login is a go

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 let app = angular.module('AudioApp', ['ui.router']);
 
-
-
 app.config(function ($stateProvider) {
     $stateProvider.state({
         name: "home",
@@ -25,51 +23,68 @@ app.component('homecomponent', {
 });
 app.component('songscomponent', {
     templateUrl: 'components/songs.html',
-       controller: 'songsController',
+    controller: 'songsController',
 });
 app.component('friendscomponent', {
     templateUrl: 'components/friends.html',
     controller: 'friendsController',
 });
-
+app.controller("LoginController", function ($scope, loginservice){
+    $scope.name = loginservice.userz();
+    $scope.createUN = function(input) {
+        loginservice.setUser(input);
+    };
+})
 app.controller("songsController", function ($scope, StuffService) {
-    console.log('asd')
     $scope.songs = StuffService.songz();
 });
 app.controller("friendsController", function ($scope, StuffService) {
     $scope.friends = StuffService.friendz();
 });
+app.factory('loginservice', function() {
+    let user = {
+        name: ""
+    }
+    return {
+        userz: function () {
+            return user.name;
+        },
 
-app.factory('StuffService', function(){
-    let songs = [{
+        setUser: function (input) {
+            user.name = input;
+        }
+    }
+})
+app.factory('StuffService', function () {
+    let songs = [{ 
         title: "asdasdasd",
         artist: "afndoufd"
     },
-    {
-        title: "kbiudbfib",
-        artist: "sdviybibv"
-    },
-    {
-        title: "oisdnvoubfub",
-        artist: "zowfosubdgon",
-    }];
+        {
+            title: "kbiudbfib",
+            artist: "sdviybibv"
+        },
+        {
+            title: "oisdnvoubfub",
+            artist: "zowfosubdgon",
+        }];
     let friends = [{
         name: "frank",
         songsliked: 3,
     },
-    {
-        name: "jumbo joe",
-        songsliked: 98
-    },
-    {
-        name: "Landotron 2000",
-        songsliked: 0
-    }]
+        {
+            name: "jumbo joe",
+            songsliked: 98
+        },
+        {
+            name: "Landotron 2000",
+            songsliked: 0
+        }]
     return {
-        songz: function(){
+        songz: function () {
             return songs;
         },
-        friendz: function(){
+        friendz: function () {
             return friends;
         }
     }
--- public/app.js
+++ public/app.js
@@ -1,8 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 let app = angular.module('AudioApp', ['ui.router']);
 
-
-
 app.config(function ($stateProvider) {
     $stateProvider.state({
         name: "home",
@@ -26,51 +24,68 @@ app.component('homecomponent', {
 });
 app.component('songscomponent', {
     templateUrl: 'components/songs.html',
-       controller: 'songsController',
+    controller: 'songsController',
 });
 app.component('friendscomponent', {
     templateUrl: 'components/friends.html',
     controller: 'friendsController',
 });
-
+app.controller("LoginController", function ($scope, loginservice){
+    $scope.name = loginservice.userz();
+    $scope.createUN = function(input) {
+        loginservice.setUser(input);
+    };
+})
 app.controller("songsController", function ($scope, StuffService) {
-    console.log('asd')
     $scope.songs = StuffService.songz();
 });
 app.controller("friendsController", function ($scope, StuffService) {
     $scope.friends = StuffService.friendz();
 });
+app.factory('loginservice', function() {
+    let user = {
+        name: ""
+    }
+    return {
+        userz: function () {
+            return user.name;
+        },
 
-app.factory('StuffService', function(){
-    let songs = [{
+        setUser: function (input) {
+            user.name = input;
+        }
+    }
+})
+app.factory('StuffService', function () {
+    let songs = [{ 
         title: "asdasdasd",
         artist: "afndoufd"
     },
-    {
-        title: "kbiudbfib",
-        artist: "sdviybibv"
-    },
-    {
-        title: "oisdnvoubfub",
-        artist: "zowfosubdgon",
-    }];
+        {
+            title: "kbiudbfib",
+            artist: "sdviybibv"
+        },
+        {
+            title: "oisdnvoubfub",
+            artist: "zowfosubdgon",
+        }];
     let friends = [{
         name: "frank",
         songsliked: 3,
     },
-    {
-        name: "jumbo joe",
-        songsliked: 98
-    },
-    {
-        name: "Landotron 2000",
-        songsliked: 0
-    }]
+        {
+            name: "jumbo joe",
+            songsliked: 98
+        },
+        {
+            name: "Landotron 2000",
+            songsliked: 0
+        }]
     return {
-        songz: function(){
+        songz: function () {
             return songs;
         },
-        friendz: function(){
+        friendz: function () {
             return friends;
         }
     }
